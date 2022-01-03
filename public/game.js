b081c7015d3c5fb2e5254ff235e1ae498a4f9eb7
Romain Lenoir <romain-l58@hotmail.fr>
2022-01-03 15:22:27 +0100

    Move Player

--- public/game.js
+++ public/game.js
@@ -20,9 +20,34 @@ function drawPlayers() {
 //Loop draw, call 60 times per second
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height); //met en noir transparent tous les pixels dans le rectangle défini par le point de départ de coordonnées (x, y) et par les tailles _(largeur, hauteur)*, supprimant tout contenu précédemment dessiné.
+    movePlayer();
     drawPlayers();
     requestAnimationFrame(update); //notifie le navigateur que vous souhaitez exécuter une animation et demande que celui-ci exécute une fonction spécifique de mise à jour de l'animation
 }
 
 //First call
 requestAnimationFrame(update);
+
+
+// ------ KEYBOARD ------ 
+const keyboard = {};
+
+window.onkeydown = function(e){
+    keyboard[e.key] = true;
+};
+
+window.onkeyup = function(e){
+    delete keyboard[e.key];
+};
+//Cette implémentation nous permet de détecter l'appui de plusieurs touches simultanément. 
+
+
+//Function move Player
+function movePlayer(){
+    if (keyboard['ArrowLeft']) player.x -= player.speed; // left
+    if (keyboard['ArrowUp']) player.y -= player.speed; // up
+    if (keyboard['ArrowRight']) player.x += player.speed; // right
+    if (keyboard['ArrowDown']) player.y += player.speed; // down
+}
+
+
