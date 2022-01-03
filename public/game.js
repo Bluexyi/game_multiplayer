5baaaae45a94ce5f2da5772aadb5e3e5893f18ba
Romain Lenoir <romain-l58@hotmail.fr>
2022-01-03 15:43:40 +0100

    multiplayers OK

--- public/game.js
+++ public/game.js
@@ -1,20 +1,27 @@
+const socket = io();
+
 //Player variables
-const player = {
-    x: 0,
-    y: 0,
-    size: 20,
-    speed: 5
-};
+//const player = {
+//    x: 0,
+//    y: 0,
+//    size: 20,
+//    speed: 5
+//};
+
+//Player list
+let players = [];
 
 //The Game is draw in Canvas HTML element
 const ctx = canvas.getContext('2d');
 
-//Fonction draw the player (Carre de cotes size)
+//Fonction draw all players (Carre de cotes size)
 function drawPlayers() {
-    const {x, y, size} = player;
-    ctx.beginPath(); //Creer un nouveau chemin
-    ctx.rect(x, y, size, size); //Creer chemin en forme de rectangle
-    ctx.fill(); //Remplit le chemin avec la couleur de fond en cours
+    players.forEach(function({x, y, size, c}) {
+        ctx.beginPath(); //Creer un nouveau chemin
+        ctx.rect(x, y, size, size); //Creer chemin en forme de rectangle
+        ctx.fill(); //Remplit le chemin avec la couleur de fond en cours    
+    })
+    //const {x, y, size} = player;
 }
 
 //Loop draw, call 60 times per second
@@ -44,10 +51,21 @@ window.onkeyup = function(e){
 
 //Function move Player
 function movePlayer(){
-    if (keyboard['ArrowLeft']) player.x -= player.speed; // left
-    if (keyboard['ArrowUp']) player.y -= player.speed; // up
-    if (keyboard['ArrowRight']) player.x += player.speed; // right
-    if (keyboard['ArrowDown']) player.y += player.speed; // down
+//    if (keyboard['ArrowLeft']) player.x -= player.speed; // left
+//    if (keyboard['ArrowUp']) player.y -= player.speed; // up
+//    if (keyboard['ArrowRight']) player.x += player.speed; // right
+//    if (keyboard['ArrowDown']) player.y += player.speed; // down
+
+    //informe le serveur qu'un mouvement doit se produire
+    if (keyboard['ArrowLeft']) socket.emit('move left');
+    if (keyboard['ArrowUp']) socket.emit('move up');
+    if (keyboard['ArrowRight']) socket.emit('move right');
+    if (keyboard['ArrowDown']) socket.emit('move down');
+
 }
 
+//Écoutez l'événement players list et mettez à jour la liste des joueurs à chaque fois qu'il survient. 
+socket.on('players list', function(list){
+    players = list;
+})
 
